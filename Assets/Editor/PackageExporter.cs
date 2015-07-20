45f010cc3eb9030530ed32c13130bfbefb86174a
Edward Rowe <edward.l.rowe@gmail.com>
2015-07-20 16:15:10 -0400

    Added Package Exporter and exported asset package.

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PackageExporter : UnityEditor.EditorWindow {
+
+	static string assetPathName = "Assets/RBScripts";
+
+	[MenuItem ("PackageExporter/Export as Package")]
+	public static void ExportPRBScripts ()
+	{
+		AssetDatabase.ExportPackage (assetPathName, "RBScripts.unitypackage", ExportPackageOptions.Recurse |
+		                             ExportPackageOptions.IncludeDependencies);
+		Debug.Log ("Exported!");
+	}
+}
