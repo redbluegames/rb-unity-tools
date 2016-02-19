ebc5bfd1542dfa5b1fc356405d3422991564e067
Edward Rowe <edward.l.rowe@gmail.com>
2016-02-19 01:36:23 -0500

    Update Singleton with XML comments

--- Assets/RedBlueGames/RBScripts/Singleton.cs
+++ Assets/RedBlueGames/RBScripts/Singleton.cs
@@ -25,71 +25,77 @@ using UnityEngine;
  */ 
 namespace RedBlueGames.Tools
 {
+	using UnityEngine;
+
+	/// <summary>
+	/// Be aware this will not prevent a non singleton constructor
+	///   such as `T myT = new T();`
+	/// To prevent that, add `protected T () {}` to your singleton class.
+	/// 
+	/// As a note, this is made as MonoBehaviour because we need Coroutines.
+	/// </summary>
 	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 	{
+		private static object _lock = new object();
+		private static bool applicationIsQuitting = false;
+
 		private static T _instance;
-		private static object _lock = new object ();
- 
-		public static T Instance {
-			get {
+		public static T Instance
+		{
+			get
+			{
 				if (applicationIsQuitting) {
-					Debug.LogWarning ("[Singleton] Instance '" + typeof(T) +
+					Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
 						"' already destroyed on application quit." +
 						" Won't create again - returning null.");
 					return null;
 				}
- 
-				lock (_lock) {
-					if (_instance == null) {
-						_instance = (T)FindObjectOfType (typeof(T));
- 
-						if (FindObjectsOfType (typeof(T)).Length > 1) {
-							Debug.LogError ("[Singleton] Something went really wrong " +
+
+				lock(_lock)
+				{
+					if (_instance == null)
+					{
+						_instance = (T) FindObjectOfType(typeof(T));
+
+						if ( FindObjectsOfType(typeof(T)).Length > 1 )
+						{
+							Debug.LogError("[Singleton] Something went really wrong " +
 								" - there should never be more than 1 singleton!" +
-								" Reopenning the scene might fix it.");
+								" Reopening the scene might fix it.");
 							return _instance;
 						}
- 
-						if (_instance == null) {
-							GameObject singleton = new GameObject ();
-							_instance = singleton.AddComponent<T> ();
-							singleton.name = "(singleton) " + typeof(T).ToString ();
- 
-							DontDestroyOnLoad (singleton);
- 
-							Debug.Log ("[Singleton] An instance of " + typeof(T) + 
+
+						if (_instance == null)
+						{
+							GameObject singleton = new GameObject();
+							_instance = singleton.AddComponent<T>();
+							singleton.name = "(singleton) "+ typeof(T).ToString();
+
+							DontDestroyOnLoad(singleton);
+
+							Debug.Log("[Singleton] An instance of " + typeof(T) + 
 								" is needed in the scene, so '" + singleton +
 								"' was created with DontDestroyOnLoad.");
 						} else {
-							Debug.Log ("[Singleton] Using instance already created: " +
+							Debug.Log("[Singleton] Using instance already created: " +
 								_instance.gameObject.name);
 						}
 					}
- 
+
 					return _instance;
 				}
 			}
 		}
-		
-		static bool applicationIsQuitting = false;
-		
-		// WORKAROUND: Allow bool to be reflagged so that Unit Tests can set up singletons in editor.
-		public static void ResetSingleton ()
-		{
-			applicationIsQuitting = false;
-		}
 
-		/*
-		 * When Unity quits, it destroys objects in a random order.
-		 * In principle, a Singleton is only destroyed when application quits.
-		 * 
-		 * If any script calls Instance after it have been destroyed, 
-		 * it will create a buggy ghost object that will stay on the Editor scene
-		 * even after stopping playing the Application. Really bad!
-		 * So, this was made to be sure we're not creating that buggy ghost object.
-		 */
-		public virtual void OnApplicationQuit ()
-		{
+		/// <summary>
+		/// When Unity quits, it destroys objects in a random order.
+		/// In principle, a Singleton is only destroyed when application quits.
+		/// If any script calls Instance after it have been destroyed, 
+		///   it will create a buggy ghost object that will stay on the Editor scene
+		///   even after stopping playing the Application. Really bad!
+		/// So, this was made to be sure we're not creating that buggy ghost object.
+		/// </summary>
+		public virtual void OnDestroy () {
 			applicationIsQuitting = true;
 		}
 	}
