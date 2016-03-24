b260c5d9d44e57f1b552164bbfb9ae4bd1bd40f7
Edward Rowe <edward.l.rowe@gmail.com>
2016-03-24 18:13:47 -0400

    Comply AnimBuilder with StyleCop

---
+++ Assets/RedBlueGames/RBScripts/Editor/AnimationUtilities/AnimBuilderEditor.cs
@@ -0,0 +1,65 @@
+﻿namespace RedBlueGames.Tools
+{
+    using UnityEditor;
+    using UnityEngine;
+    using System.Collections;
+
+    [CustomEditor(typeof(AnimBuilder))]
+    public class AnimBuilderEditor : UnityEditor.Editor
+    {
+        string clipName;
+        bool allowErasing = false;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Generate Clips"))
+            {
+                AnimBuilder animBuilder = (AnimBuilder)target;
+                animBuilder.GenerateClips();
+            }
+
+            clipName = EditorGUILayout.TextField("Clip Name", clipName);
+            if (GUILayout.Button("Add Eight Dir Clips"))
+            {
+                AnimBuilder animBuilder = (AnimBuilder)target;
+                animBuilder.AddEightBlendToClips(clipName);
+            }
+
+            // Enable this only for when you duplicate a builder and need to clear it... should rarely come up.
+            allowErasing = EditorGUILayout.BeginToggleGroup("EnableEraseClips", allowErasing);
+            if (GUILayout.Button("Erase clips"))
+            {
+                AnimBuilder animBuilder = (AnimBuilder)target;
+                foreach (AnimBuilder.SpriteAnimClip clip in animBuilder.Clips)
+                {
+                    clip.SavedClip = null;
+                }
+            }
+            EditorGUILayout.EndToggleGroup();
+        }
+    }
+
+    public static class AnimBuilderCreator
+    {
+        [MenuItem("Assets/Create/AnimBuilder/Empty")]
+        static void CreateAnimBuilder()
+        {
+            AnimBuilder animBuilder = ScriptableObjectUtility.CreateAsset <AnimBuilder>();
+            animBuilder.Initialize();
+
+            AssetDatabase.SaveAssets();
+        }
+
+        [MenuItem("Assets/Create/AnimBuilder/Character")]
+        static void CreateAnimBuilderForCharacter()
+        {
+            AnimBuilder animBuilder = ScriptableObjectUtility.CreateAsset <AnimBuilder>();
+            animBuilder.Initialize();
+            animBuilder.InitializeForCharacter();
+
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Assets/RedBlueGames/RBScripts/Editor/ScriptableObjectUtility.cs
@@ -0,0 +1,34 @@
+﻿namespace RedBlueGames.Tools
+{
+    using System.Collections;
+    using System.IO;
+    using RedBlueGames.Tools;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Utility functions for ScriptableObjects.
+    /// </summary>
+    public static class ScriptableObjectUtility
+    {
+        /// <summary>
+        /// Creates and saves the Scriptable Object as an asset in the Editor. Adapted from Unity3D wiki:
+        /// http://wiki.unity3d.com/index.php?title=CreateScriptableObjectAsset
+        /// </summary>
+        /// <typeparam name="T">The type of the Scriptable Object to create.</typeparam>
+        public static T CreateAsset<T>() where T : ScriptableObject
+        {
+            T scriptableObjectInstance = ScriptableObject.CreateInstance<T>();
+
+            string currentPath = SelectionUtilities.GetDirectoryOfSelection();
+            string path = currentPath;
+            string typeName = typeof(T).ToString();
+            string typeWithoutNamespace = typeName.Split('.').Last();
+            string uniqueAssetPath = AssetDatabase.GenerateUniqueAssetPath(path + "New " + typeWithoutNamespace + ".asset");
+
+            AssetDatabaseUtility.SaveAndSelectObject(scriptableObjectInstance, currentPath, Path.GetFileName(uniqueAssetPath));
+
+            return scriptableObjectInstance;
+        }
+    }
+}
