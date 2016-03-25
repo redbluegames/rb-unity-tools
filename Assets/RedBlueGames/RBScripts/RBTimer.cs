e715c2a2770c356888122650a8a9ee601c638e7c
Edward Rowe <edward.l.rowe@gmail.com>
2016-03-25 17:03:59 -0400

    Comply RBTimer with StyleCop

--- Assets/RedBlueGames/RBScripts/RBTimer.cs
+++ Assets/RedBlueGames/RBScripts/RBTimer.cs
@@ -19,53 +19,72 @@ namespace RedBlueGames.Tools
     using System.Collections;
     using UnityEngine;
 
-    /* Timer adapted from Adam Winkels (Lovers in a Dangerous Spacetime)
- * https://gist.githubusercontent.com/winkels/779ef0bdba4f88edeceb/raw/CoroutineTimer.cs
- */
+    /// <summary>
+    /// Timer adapted from Adam Winkels (Lovers in a Dangerous Spacetime)
+    /// https://gist.githubusercontent.com/winkels/779ef0bdba4f88edeceb/raw/CoroutineTimer.cs
+    /// </summary>
     [System.Serializable]
     public class RBTimer
     {
-        ///public fields
-        public float Duration;
-        public bool Repeats;
-        public bool UsesFixedUpdate;
+        [SerializeField]
+        private float duration;
+        [SerializeField]
+        private bool repeats;
+        [SerializeField]
+        private bool usesFixedUpdate;
 
-        ///private fields
-        private float TimeRemaining;
-        private bool _isRunning;
+        private float timeRemaining;
+        private bool isRunning;
 
         private IEnumerator timerCoroutine;
         private MonoBehaviour targetMonobehaviour;
         private System.Action timerFinishedAction;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RBTimer"/> class without a Duration
+        /// </summary>
+        public RBTimer()
+        {
+            this.duration = float.NaN;
+            this.repeats = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RBTimer"/> class with a duration.
+        /// </summary>
+        /// <param name="timerDuration">Timer duration.</param>
+        /// <param name="repeats">If set to <c>true</c>, timer will repeat after it completes.</param>
+        public RBTimer(float timerDuration, bool repeats = false)
+        {
+            this.duration = timerDuration;
+            this.repeats = repeats;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this timer is currently counting down.
+        /// </summary>
+        /// <value><c>true</c> if this instance is running; otherwise, <c>false</c>.</value>
         public bool IsRunning
         {
             get
             {
-                return _isRunning;
+                return this.isRunning;
             }
         }
 
-        public RBTimer()
-        {
-            this.Duration = float.NaN;
-            this.Repeats = false;
-        }
-
-        public RBTimer(float timerDuration, bool repeats = false)
-        {
-            this.Duration = timerDuration;
-            this.Repeats = repeats;
-        }
-
+        /// <summary>
+        /// Start the timer, attached to the specified targetMonobehaviour. Raises timerFinishedAction when complete.
+        /// </summary>
+        /// <param name="targetMonobehaviour">Target monobehaviour to attach Coroutine to.</param>
+        /// <param name="timerFinishedAction">Action invoked on timer complete.</param>
         public void Start(MonoBehaviour targetMonobehaviour, System.Action timerFinishedAction = null)
         {
-            if (IsUnset())
+            if (this.IsUnset())
             {
                 throw new System.InvalidOperationException("Trying to start a timer without setting its duration.");
             }
 
-            if (timerCoroutine != null)
+            if (this.timerCoroutine != null)
             {
                 throw new System.InvalidOperationException(
                     "This timer has already been started. If you are trying to Start it during timerAction use Repeats flag.");
@@ -74,85 +93,98 @@ namespace RedBlueGames.Tools
             this.timerFinishedAction = timerFinishedAction;
             this.targetMonobehaviour = targetMonobehaviour;
 
-            DoStart();
+            this.DoStart();
         }
 
+        /// <summary>
+        /// Determines whether this instance is unset.
+        /// </summary>
+        /// <returns><c>true</c> if this instance is unset; otherwise, <c>false</c>.</returns>
         public bool IsUnset()
         {
-            return float.IsNaN(Duration);
+            return float.IsNaN(this.duration);
         }
 
-        void DoStart()
+        /// <summary>
+        /// Gets the time remaining.
+        /// </summary>
+        /// <returns>The time remaining.</returns>
+        public float GetTimeRemaining()
         {
-            if (Duration <= 0.0f)
+            return this.timeRemaining;
+        }
+
+        /// <summary>
+        /// Stop this timer, without calling the Finished action.
+        /// </summary>
+        public void Stop()
+        {
+            this.isRunning = false;
+
+            if (this.timerCoroutine != null)
+            {
+                this.StopTimerCoroutine();
+            }
+
+            // Clean up references
+            this.timeRemaining = 0.0f;
+            this.targetMonobehaviour = null;
+            this.timerFinishedAction = null;
+        }
+
+        private void DoStart()
+        {
+            if (this.duration <= 0.0f)
             {
                 throw new System.InvalidOperationException("Timer duration cannot be less than or equal to zero");
             }
 
-            StartTimerCoroutine(Duration);
+            this.StartTimerCoroutine(this.duration);
 
-            _isRunning = true;
+            this.isRunning = true;
         }
 
-        void TimerFinished()
+        private void TimerFinished()
         {
-            _isRunning = false;
+            this.isRunning = false;
+
             // Fire time finished event
-            if (timerFinishedAction != null)
+            if (this.timerFinishedAction != null)
             {
-                timerFinishedAction();
+                this.timerFinishedAction();
             }
 
-            //null check to make sure the timer has not been stopped in the timerFinishedDelegate
-            if (Repeats && timerCoroutine != null)
+            // Null check to make sure the timer has not been stopped in the timerFinishedDelegate
+            if (this.repeats && this.timerCoroutine != null)
             {
-                DoStart();
+                this.DoStart();
             }
             else
             {
-                Stop();
+                this.Stop();
             }
-        }
-
-        public float GetTimeRemaining()
-        {
-            return TimeRemaining;
-        }
-
-        public void Stop()
-        {
-            _isRunning = false;
-
-            if (timerCoroutine != null)
-            {
-                StopTimerCoroutine();
-            }
-
-            // Clean up references
-            TimeRemaining = 0.0f;
-            targetMonobehaviour = null;
-            timerFinishedAction = null;
         }
 
         #region TimerCoroutine
 
         private void StartTimerCoroutine(float waitSeconds)
         {
-            timerCoroutine = CountdownForDuration(waitSeconds);
-            targetMonobehaviour.StartCoroutine(timerCoroutine);
+            this.timerCoroutine = this.CountdownForDuration(waitSeconds);
+            this.targetMonobehaviour.StartCoroutine(this.timerCoroutine);
         }
 
         private IEnumerator CountdownForDuration(float desiredDuration)
         {
             /* Note: If we use a ton of timers, we may need to use this simpler version (shown)
-			 * But for now, let's use a version that we can see how much time is remaining.
-			 * yield return new WaitForSeconds (waitSeconds); */
-            TimeRemaining += desiredDuration;
+             * But for now, let's use a version that we can see how much time is remaining.
+             * yield return new WaitForSeconds (waitSeconds);
+             */
+            this.timeRemaining += desiredDuration;
             while (true)
             {
                 // yield at the start of the loop to always wait at least one frame.
                 // Only a <= 0 second timer would finish immediately, and that's not allowed.
-                if (UsesFixedUpdate)
+                if (this.usesFixedUpdate)
                 {
                     // FixedUpdate timers are used to guarantee precision on physics objects like projectiles.
                     yield return new WaitForFixedUpdate();
@@ -162,20 +194,20 @@ namespace RedBlueGames.Tools
                     yield return null;
                 }
 
-                TimeRemaining -= Time.deltaTime;
-                if (TimeRemaining <= 0.0f)
+                this.timeRemaining -= Time.deltaTime;
+                if (this.timeRemaining <= 0.0f)
                 {
                     break;
                 }
             }
 
-            TimerFinished();
+            this.TimerFinished();
         }
 
         private void StopTimerCoroutine()
         {
-            targetMonobehaviour.StopCoroutine(timerCoroutine);
-            timerCoroutine = null;
+            this.targetMonobehaviour.StopCoroutine(this.timerCoroutine);
+            this.timerCoroutine = null;
         }
 
         #endregion
